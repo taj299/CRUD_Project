3f971a1f991d7e7ee569312130f76eb4e05a8607
taj299 <shaikhtajoddin299@gmail.com>
2025-01-30 12:07:12 +0530

    changes

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("indexAgain/",views.index, name="indexAgain"),
+    path("indexagain/",views.indexagain, name="indexagain"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,5 +6,5 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
-def index(request):
+def indexagain(request):
     return HttpResponse("Hello, world. You're at the polls index again")
